28f332f184ed1d2d37bf837f66d47a5ce1a6d3f5
ASH-PC\Admin <amr@flowplan.dk>
2022-08-10 07:35:05 +0200

    #0 - init deploy

---
+++ bin/deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+DEPLOYMENT_NUMBER="$(date -u +%FT%TZ)"
+echo "$DEPLOYMENT_NUMBER" > "$DIR/../deployment_number"
+
+PROJECT_NAME="$(cat "$DIR/../infrastructure/variables.tf" | grep 'project_name' -A 2 | grep 'default' | cut -d '=' -f 2 | cut -d '"' -f 2)"
+
+pushd "$DIR/../backend/"
+  npm i
+  npm run build
+  pushd build
+    zip -r rest_api.zip ./
+    aws s3 cp ./rest_api.zip "s3://$PROJECT_NAME-backend/$DEPLOYMENT_NUMBER/"
+  popd
+popd
+
+pushd "$DIR/../infrastructure"
+  terraform init
+  terraform apply -auto-approve -var deployment_number="$DEPLOYMENT_NUMBER"
+popd
---
+++ infrastructure/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  type    = string
+  default = "dynamic-form"
+}
+
+variable "deployment_number" {
+  type    = string
+  default = "initial"
+}
