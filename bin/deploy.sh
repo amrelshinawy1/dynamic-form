131d2be42ce0d0e0ae55c11de48fc3ad5acc5bf2
ASH-PC\Admin <amr@flowplan.dk>
2022-08-11 20:52:47 +0200

    #0 - frontend

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,9 +5,17 @@ echo "$DEPLOYMENT_NUMBER" > "$DIR/../deployment_number"
 
 PROJECT_NAME="$(cat "$DIR/../infrastructure/variables.tf" | grep 'project_name' -A 2 | grep 'default' | cut -d '=' -f 2 | cut -d '"' -f 2)"
 
-pushd "$DIR/../backend/"
+pushd "$DIR/../frontend"
   npm i
   npm run build
+  aws s3 cp --recursive --acl public-read build/ "s3://$PROJECT_NAME-frontend/"
+popd
+
+[ "$1" == "frontend" ] && exit 0
+
+pushd "$DIR/../backend/"
+  npm run build
+  npm run test
   pushd build
     zip -r rest_api.zip ./
     aws s3 cp ./rest_api.zip "s3://$PROJECT_NAME-backend/$DEPLOYMENT_NUMBER/"
--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,3 +1,23 @@
+resource "aws_s3_bucket" "frontend" {
+  bucket        = "${var.project_name}-frontend"
+  force_destroy = "false"
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+  acl           = "public-read"
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+
 resource "aws_s3_bucket" "backend" {
   bucket        = "${var.project_name}-backend"
   force_destroy = "false"
